c063dd3eea3179c06571d186c2a27fec08f60d68
Jameson Farmer <thejamesonfarmer@gmail.com>
2025-04-05 11:18:49 -0700

    Added Functionality to display reviews on the page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { calculateStarAverage } from "./logic.js";
 //Do not change //////////////////////////////////
 const reviews = [
   {
@@ -23,3 +24,43 @@ const reviews = [
 /////////////////////////////////////////////////////////////////////
 
 //Your Code Below Here////
+
+window.onload = () => {
+  loadReviews(); 
+} 
+
+
+function loadReviews() {
+  const reviewArea = document.querySelector(".reviews");
+  for(let review of reviews){
+    //console.log(review);
+    addElement(review, reviewArea);
+  }
+}
+
+function addElement(review, reviewArea){
+  const container = document.createElement('div');
+  container.className = "review_container"; 
+
+  const img = document.createElement("img");
+  img.src = review.image; 
+
+  const textDiv = document.createElement("div");
+
+  const username = document.createElement("p");
+  username.textContent = review.username;
+
+  const stars = document.createElement("p");
+  stars.textContent = `${review.star} stars`;
+
+  const reviewinfo = document.createElement("p");
+  reviewinfo.textContent = review.review; 
+  textDiv.appendChild(username);
+  textDiv.appendChild(stars);
+  textDiv.appendChild(reviewinfo); 
+
+  container.appendChild(img);
+  container.appendChild(textDiv);
+
+  reviewArea.appendChild(container);
+}
